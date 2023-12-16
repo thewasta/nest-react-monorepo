232b8360702883caf8ece566542306fc04c8587b
Schenier Lopez <schnierlopez@gmail.com>
2023-12-16 23:58:21 +0100

    Remove files and start with empty Component

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,8 @@
 function App() {
     return (
         <>
-            <button onClick={async () => {
-                const response = await fetch('/api')
-                const parse = await response.json()
-                console.log(parse);
-            }}>
-                Click Me
+            <button>
+                Click Me!
             </button>
         </>
     )

1e0c2619ee5cf8ef23e6bf3ff9588f23bb721445
Schenier Lopez <schnierlopez@gmail.com>
2023-12-17 00:28:50 +0100

    Base components

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,15 @@
+import TaskList from "./components/TaskList.tsx";
+import TaskForm from "./components/TaskForm.tsx";
+
 function App() {
     return (
-        <>
-            <button>
-                Click Me!
-            </button>
-        </>
+        <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+            <div className="bg-gray-950 p-4 w1/5">
+                <h1 className="text-3xl font-bold text-center block my-2">Applicación de tareas</h1>
+                <TaskForm/>
+                <TaskList/>
+            </div>
+        </div>
     )
 }
 
---
+++ apps/client/src/components/TaskList.tsx
@@ -0,0 +1,7 @@
+function TaskList() {
+    return (
+        <div>Task List</div>
+    );
+}
+
+export default TaskList;

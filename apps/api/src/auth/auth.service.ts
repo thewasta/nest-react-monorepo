d75d7c9acc4f5a9a726502bcdd0e59cd79faddfc
Schenier Lopez <schnierlopez@gmail.com>
2023-12-17 22:23:19 +0100

    Setup sign up controller

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
+import {AuthDto} from "./dto/auth.dto";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {
     }
 
-    async signup() {
-        return {message: 'Signup success'}
+    async signup(dto: AuthDto) {
+        const foundUser = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        })
+        if (foundUser) {
+            throw new BadRequestException('Email already taken');
+        }
+
+        const hashedPassword = await this.hashPassword(dto.password)
+        await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hashedPassword: hashedPassword
+            }
+        })
+        return {message: 'Signup success', hashed: hashedPassword}
     }
 
     async signin() {
@@ -16,5 +34,10 @@ export class AuthService {
 
     async signout() {
         return {message: 'signout success'}
+    }
+
+    async hashPassword(password: string) {
+        const saltOrRound = 10;
+        return await bcrypt.hash(password, saltOrRound);
     }
 }

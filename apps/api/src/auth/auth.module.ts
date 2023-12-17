f986ea72142b79744fd40c6adca825dd6e5040de
Schenier Lopez <schnierlopez@gmail.com>
2023-12-17 23:57:06 +0100

    Implement JWT strategy and guard

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import {AuthService} from './auth.service';
 import {AuthController} from './auth.controller';
 import {JwtModule} from "@nestjs/jwt";
 import * as process from "process";
+import {PassportModule} from "@nestjs/passport";
 
 @Module({
-    imports: [JwtModule.register({
+    imports: [
+        PassportModule,
+        JwtModule.register({
         global: true,
         secret: process.env.JWT_SECRET,
         signOptions: {expiresIn: '1h'}
---
+++ apps/api/src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import {PassportStrategy} from "@nestjs/passport";
+import {ExtractJwt, Strategy} from "passport-jwt";
+import {Request} from 'express'
+import * as process from "process";
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                JwtStrategy.extractJWT,
+            ]),
+            secretOrKey: process.env.JWT_SECRET
+        });
+    }
+
+    private static extractJWT(req: Request): string | null {
+        if (req.cookies && 'token' in req.cookies) {
+            return req.cookies.token;
+        }
+        return null;
+    }
+
+    async validate(payload: { id: string, email: string }) {
+        return payload;
+    }
+}
